ec4e6b897d35b4686e803028eb5e02274235b14d
Siydulislam <siydulislam333@gmail.com>
2022-09-29 12:11:15 +0600

    created navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,19 @@
+import About from "./pages/About/About";
+import Home from "./pages/Home/Home";
+import Navbar from "./pages/shared/Navbar";
+import { Routes, Route } from "react-router-dom";
+import Login from "./pages/Login/Login";
+
 function App() {
   return (
-    <></>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </>
   );
 }
 
---
+++ frontend/src/pages/shared/Navbar.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Navbar = () => {
+
+    const menuItems = (
+        <>
+            <li><Link to="/" className="rounded-lg text-xl">Home</Link></li>
+            <li><Link to="/appoinment" className="rounded-lg text-xl">Appointment</Link></li>
+            <li><Link to="/review" className="rounded-lg text-xl">Review</Link></li>
+            <li><Link to="/about" className="rounded-lg text-xl">About</Link></li>
+            <li><Link to="/contuct" className="rounded-lg text-xl">Contact</Link></li>
+            <li><Link to="/login" className="rounded-lg text-xl">Login</Link></li>
+        </>
+    );
+
+    return (
+        <div className="navbar bg-base-100">
+            <div className="navbar-start">
+                <div className="dropdown">
+                    <label tabIndex={0} className="btn btn-ghost lg:hidden">
+                        <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+                    </label>
+                    <ul tabIndex={0} className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52 gap-y-2">
+                        {menuItems}
+                    </ul>
+                </div>
+                <div>
+                    <Link to="/" className="normal-case text-xl p-2">Doctors Chamber</Link>
+                </div>
+            </div>
+            <div className="navbar-end hidden lg:flex">
+                <ul className="menu menu-horizontal gap-x-2">
+                    {menuItems}
+                </ul>
+            </div>
+        </div>
+    );
+};
+
+export default Navbar;
