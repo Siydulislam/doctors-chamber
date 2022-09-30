aee3faa22cd7df0e2f46ac74adc851f5c570c56a
Siydulislam <siydulislam333@gmail.com>
2022-09-30 13:27:00 +0600

    set contact section

--- frontend/src/pages/Home/Home.js
+++ frontend/src/pages/Home/Home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Banner from './Banner';
+import Contact from './Contact';
 import Footer from './Footer';
 import Info from './Info';
 import MakeAppointment from './MakeAppointment';
@@ -16,6 +17,7 @@ const Home = () => {
             <Treatment />
             <MakeAppointment />
             <Testimonials />
+            <Contact />
             <Footer />
         </div>
     );
--- frontend/src/pages/shared/Navbar.js
+++ frontend/src/pages/shared/Navbar.js
@@ -15,7 +15,7 @@ const Navbar = () => {
     );
 
     return (
-        <div className="navbar bg-base-100 px-12">
+        <div className="navbar text-white bg-gradient-to-br from-primary to-secondary px-12">
             <div className="navbar-start">
                 <div className="dropdown">
                     <label tabIndex={0} className="btn btn-ghost lg:hidden">
